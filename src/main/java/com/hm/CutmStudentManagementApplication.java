0dbed994e0f920a2387af5114e613ff5f1c9a046
minnukota381 <minnukota381@gmail.com>
2025-05-02 01:07:42 +0530

    first commit

---
+++ src/main/java/com/hm/CutmStudentManagementApplication.java
@@ -0,0 +1,13 @@
+package com.hm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CutmStudentManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CutmStudentManagementApplication.class, args);
+	}
+
+}
